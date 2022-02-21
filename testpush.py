41644e88456586eb5a99f213f6e6074c8456260e
owingst <owingst@gmail.com>
2022-02-21 11:17:50 -0600

    modified testpush to include another soundfile

--- testpush.py
+++ testpush.py
@@ -12,22 +12,23 @@ import subprocess
 import sys
 import json
 
-def push(token, pushtopic, certfile):
+def push(token):
     """ Send Push Notifications """
 
     msg = "Hi Tim"
 
     try:
 
-        x = {"aps": {"alert": {"title": msg, "body": "WooHoo!"}, "sound": "Lucy-Im-home.aiff"}}
+        # x = {"aps": {"alert": {"title": msg, "body": "WooHoo!"}, "sound": "Lucy-Im-home.aiff"}}
+        x = {"aps": {"alert": {"title": msg, "body": "WooHoo!"}, "sound": "houston-we-have-a-problem.aiff"}}
         jsonMsg = json.dumps(x)
         partUrl = "https://api.sandbox.push.apple.com/3/device/"
 
         tokenUrl = partUrl + token
-        topic = "apns-topic: " + pushtopic
+        topic = "apns-topic: " +  "com.javajoe.Home"
 
         rc = subprocess.check_output(["curl", "-v", "--header", topic,  "--header", "apns-push-type: alert",
-                                        "--cert", certfile, "--key-type", "PEM", "--data", jsonMsg, "--http2",  tokenUrl])
+                                        "--cert", "/home/pi/sdr/PushCertificates.pem", "--key-type", "PEM", "--data", jsonMsg, "--http2",  tokenUrl])
 
         rc = rc.decode("utf-8")
 
@@ -41,4 +42,4 @@ def push(token, pushtopic, certfile):
         print("push: Exception is {}\n".format(e))
                 
 if __name__ == "__main__":
-    push(sys.argv[1], sys.argv[2], sys.argv[3])
+    push(sys.argv[1])
