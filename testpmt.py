6fe1286f35cf2bfa07f8c331df0bab3f8dc32bb0
owingst <owingst@gmail.com>
2022-04-23 21:32:00 -0500

    more...

--- testpmt.py
+++ testpmt.py
@@ -11,15 +11,17 @@
 import json
 import time
 import os
+import io
 import sys
 import datetime
 import requests
 from _datetime import timedelta
 from matplotlib import pyplot as plt
+from PIL import Image
 
 
 def getPMTData():
-
+    
     dataArray = []
 
     now = datetime.datetime.now()
@@ -54,9 +56,30 @@ def getPMTData():
         print("main: Request Failed! status code {}".format(r.status_code))
 
 
+def getPMTPlotData():
+    
+    dataArray = []
+
+    now = datetime.datetime.now()
+    dt = datetime.datetime.now() - datetime.timedelta(hours = 8)
+
+    r = requests.get("http://192.168.1.74:5000/getPMTPlotByDate", params={'start': dt, 'end': now})
+
+    print("response is: {}\n".format(r))
+
+    if (r.status_code == 200):
+
+        print("getPMTPlotData: request was successful!\n")
+
+
+    else:
+        print("main: Request Failed! status code {}".format(r.status_code))
+
+
 def main():
     
-    getPMTData()
+    #getPMTData()
+    getPMTPlotData()
 
 if __name__ == "__main__":
     main()
